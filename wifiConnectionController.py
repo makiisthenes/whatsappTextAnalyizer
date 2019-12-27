d7ba6b38ad03b802466abb53930621063f85b5b7
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2019-12-27 15:32:11 +0000

    Add files via upload

---
+++ wifiConnectionController.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import subprocess
+from time import sleep
+
+
+def check_connection():
+	try:
+		r = requests.get('https://www.google.com')
+		hasConnection = True
+		print('Connected to the internet')
+
+	except requests.exceptions.ConnectionError:
+		hasConnection = False
+		print('Not connected to the internet')
+
+
+proc = subprocess.Popen('cmd.exe', stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+def enable():
+	os.system('ipconfig /renew WiFi')
+
+def disable():
+	os.system('ipconfig /release WiFi')
+
+while True:
+	sleep(0.5)
+	usr = input('Type in D for disable, C for checking status and E for enabling wifi connection::').upper()
+	if usr == 'D':
+		disable()
+	elif usr == 'E':
+		enable()
+	elif usr == 'C':
+		check_connection()
+	else:
+		print('Input was not recognised, please try again...')
