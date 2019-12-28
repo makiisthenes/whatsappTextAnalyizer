797abc191017da963c3bd5af61ebed2d86134047
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2019-12-28 01:56:02 +0000

    Add files via upload
    This has been Updated to include emoji_count by both parties in analysis of chats.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # C:\Users\Michael\Documents\Coding Projects\Python PROJECTS\Random Python Projects\Trackers Software\Whatsapp Analysis\data\test.txt
 
-from emoji_names import emoji_names_x
-from emoji_names import emoji_names_y
+# C:\Users\Michael\Documents\Coding Projects\Python PROJECTS\Random Python Projects\Trackers Software\Whatsapp Analysis\data\test_total.txt
+
+from emojiNames import emoji_names_x
+from emojiNames import emoji_names_y
 import string
 import os
 import datetime
 import emoji
+from collections import Counter
 # import numpy as np
 # import matplotlib.pyplot as plt  # Horizontal bar plot
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -23,7 +26,8 @@ files = []
 # could implement the use of objects to name dates. attributes like message number...
 # main things: adding time difference between messages and what emoji was sent...
 emoji_input = ''
-
+usera_emoji_list = []
+userb_emoji_list = []
 users = []
 erafer = 0
 
@@ -54,21 +58,29 @@ def get_users():
 	print(usera)
 	print(userb)
 
-def emoji_counter():
-	pass
-	# need to make lists turn into dictionary
-
 
 def emoji_search_x(emoji_input):
-	for emoji_namex, emoji_text in emoji.EMOJI_UNICODE.items():
-		if emoji_text == emoji_input:
-			print(emoji_namex) # need to record this in something, make a new function.
+	global topUsedEmojiX
+	global userb_emoji_list
+	for emoji_namex, emoji_textx in emoji.EMOJI_UNICODE.items():
+		if emoji_textx == emoji_input:
+			print(userb_name + ' sent the emoji '+emoji_namex)
+			userb_emoji_list.append(emoji_namex)
+	topUsedEmojiX = Counter(userb_emoji_list).most_common(5)
 
 
 def emoji_search_y(emoji_input):
-	for emoji_namey, emoji_text in emoji.EMOJI_UNICODE.items():
-		if emoji_text == emoji_input:
-			print(emoji_namey) # need to record this in something, make a new function.
+	global topUsedEmojiY
+	global usera_emoji_list
+	# emoji_namey is the name given to the emoji character for user y
+	# emoji_text is emoji character that is linked to that emoji_namey.
+	for emoji_namey, emoji_texty in emoji.EMOJI_UNICODE.items():  # here we meed to check the whole dictionary to see if
+		if emoji_texty == emoji_input:
+			print(usera_name + ' sent the emoji '+ emoji.demojize(emoji_input))
+			emoji_name = emoji.demojize(emoji_input)
+			usera_emoji_list.append(emoji_name)
+	topUsedEmojiY = Counter(usera_emoji_list).most_common(5)
+	# later give option for seeing x number of top emojis used...
 
 
 def analyize_txt(textfile):
@@ -124,27 +136,28 @@ def analyize_txt(textfile):
 						if date not in unqiue_count:
 							unqiue_count.append(date)
 							line_part_count = 0
-							# print('These messages were sent on ' + date + ':')
+							if showText:
+								print('These messages were sent on ' + date + ':')
 						# this will find the unqiue date stamp for this information...
 					# print('part count '+ str(line_part_count))
 					if part_count == 1:
 						msg_time = part
-						# print('This message was sent at ' + msg_time) # want to add time difference before each message...
-						# print('	--> '+ line)
-						# print(str(line_part_count) + ' ' + str(line))
+						if showText:
+							print('This message was sent at ' + msg_time) # want to add time difference before each message...
+							print('	--> ' + line)
 					if userb in part:
 						x = x + 1
 					elif usera in part:
 						y = y + 1
 					for letters in part:
-						if letters in emoji.UNICODE_EMOJI:
+						if letters in emoji.UNICODE_EMOJI:  # place where individual letters are seen if emojis exsist...
 							emoji_count = emoji_count + 1
 							for emoji_link_checker in line.split():
 								if userb in emoji_link_checker:
-									emoji_count_x = emoji_count_x + 1
+									emoji_count_x = emoji_count_x + 1  # user 1 emoji count...
 									belongsUserA = True
 								elif usera in emoji_link_checker:
-									emoji_count_y = emoji_count_y + 1
+									emoji_count_y = emoji_count_y + 1  # user 2 emoji count...
 									belongsUserB = True
 						if belongsUserB:
 							emoji_search_x(letters)
@@ -172,6 +185,8 @@ def analyize_txt(textfile):
 	print('''
 	+-------------------------------------Analysis-------------------------------------+
 	''')
+
+	# need to make this one big print statement to be able to send to an text file...
 	print('--> '+userb_name+' has sent total: ' + str(x) + ' messages.')
 	z_state = userb_name+' has sent total: ' + str(x) + ' messages.'
 	print('--> '+usera_name+' has sent total: ' + str(y) + ' messages.')
@@ -192,7 +207,6 @@ def analyize_txt(textfile):
 		rat_eng = float((float(x) / float(word))) * 100
 	except ZeroDivisionError:
 		rat_eng = 'ERROR'
-	# print('Ratio of Engagement: {}% for all data'.format(rat_eng))
 	rat = '--> Ratio of Engagement: {}% for all data.'.format(rat_eng)
 	print(rat)
 	word = int(y - images_y) + int(x - images_x)
@@ -201,16 +215,21 @@ def analyize_txt(textfile):
 	except ZeroDivisionError:
 		rat_eng_actual = 'ERROR'
 	print('--> Ratio of Actual Engagement: {}% for text messages.'.format(rat_eng_actual))
-	print("--> Dates of data used "+ str(unqiue_count)+'.')
+	print("--> Dates of data used " + str(unqiue_count)+'.')
 	print("--> No of days of data used:: " + str(len(unqiue_count))+' days.')
-	print('--> Number of times '+userb_name+' has initiated: '+ str(init_x) + ' time(s).')
+	print('--> Number of times '+userb_name+' has initiated: ' + str(init_x) + ' time(s).')
 	print('--> Number of times '+usera_name+' has initiated: ' + str(init_y) + ' time(s).')
-	print('--> The users in this conversation is ' + usera_name + ' and ' + userb_name+ '.')
+	print('--> The users in this conversation is ' + usera_name + ' and ' + userb_name + '.')
+	print('The top 5 most used emojis by ' + userb_name + ' are ' + str(topUsedEmojiX))
+	print('The top 5 most used emojis by ' + usera_name + ' are ' + str(topUsedEmojiY))
+	# print(userb_emoji_list)
+	# print(usera_emoji_list)
 	print('''
 	''')
 	print('''
 		+----------------------------------------END---------------------------------------+
 		''')
+
 	hasCompleted = True
 
 
@@ -231,6 +250,18 @@ def date_file_labeller():
 	textname = str(dt.day) + '-' + str(dt.month) + 'textAnalysis.txt'
 	print(textname)
 
+
+print('Please be aware selecting (Y) is unstable and may not work...')
+user_opt1 = input('Do you want to see the texts[live] during analysis::: (Y)es or (N)o:: ').upper()
+while True:
+	if user_opt1 == 'Y':
+		showText = True
+		break
+	elif user_opt1 == 'N':
+		showText = False
+		break
+	else:
+		user_opt1 = input('Do you want to see the texts[live] during analysis::: (Y)es or (N)o:: ').upper()
 get_users()
 usera_name  = usera.replace(':', '')
 # print(usera)
@@ -238,7 +269,7 @@ userb_name = userb.replace(':', '')
 # print(userb)
 hasCompleted = False
 while usr_input != 1 and not hasCompleted:
-	print("To analyise the WhatsApp texts today, continue, else write 'EXIT' to exit program")
+	print("To analyse the WhatsApp texts today, continue, else write 'EXIT' to exit program")
 	try:
 		try:
 			date_file_labeller()
